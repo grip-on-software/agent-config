df6a00f51e8b4c5f303cd9f54b5566b0dd1e5930
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2018-05-28 14:48:54 +0200

    Unit tests: Add edit submission tests
    - Use a separate config, export and identity directories to avoid
      overwriting development configuration and start with a clean state
    - Test for submission as well as display after submission
    - Add test for edit submission with missing options
    - Add test for dummy source after submission
    - Bugfix in form: Retrieve deploy key for correct credentials.
      The first section was taken even if we should check that the section
      for the current component has a custom deploy key identity.

--- tests/index.js
+++ tests/index.js
@@ -3,7 +3,6 @@
 'use strict';
 
 const request = require('supertest'),
-      assert = require('chai').assert,
       jsdom = require('jsdom'),
       { JSDOM } = jsdom,
       app = require('../lib/app');
@@ -13,17 +12,5 @@ describe('Index', function() {
         request(app).get('/')
             .expect('Content-Type', 'text/html')
             .expect(200, done);
-    });
-});
-
-describe('Edit', function() {
-    it('Should provide a form', function() {
-        return request(app).get('/edit')
-            .expect('Content-Type', 'text/html')
-            .expect(200)
-            .then(response => {
-                const { document } = (new JSDOM(response.text)).window;
-                assert.equal(document.querySelectorAll(".component").length, 3);
-            });
     });
 });
